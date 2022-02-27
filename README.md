8b7be7f32bd32a7cc7bbe9a79a89a4c30e8df6de
Tarık Temel <utariktemel@gmail.com>
2022-02-27 09:40:25 +0300

    init project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # istanbulrentalvip
 Rental for istanbul
+
+```bash
+npm run dev
+# or
+yarn dev
+```
---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Istanbul Rental Vip</title>
+        <meta name="description" content="rental car, yatch, helicopter for istanbul" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        THE SITE IS IN UNDERCONSTRUCTION
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
